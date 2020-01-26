b67c05f4ffb82ea05cb14a95ba8eb1fd243920ec
Temka31 <temabichev@icloud.com>
2020-01-26 16:00:22 +0300

    z4

---
+++ z4.js
@@ -0,0 +1,31 @@
+class User {
+	constructor(name, surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+
+	getFullName() {
+		return this.name + ' ' + this.surname;
+	}
+} 
+
+class Student extends User { 
+constructor (name, surname, year){ 
+super(name, surname); 
+this.year = year; 
+} 
+getCourse() { 
+let now = new Date(), 
+nowYear = now.getFullYear(); 
+return nowYear - this.year; 
+} 
+} 
+
+
+var student = new Student('Иван', 'Иванов', 2017);
+
+console.log(student.name); //выведет 'Иван'
+console.log(student.surname); //выведет 'Иванов'
+console.log(student.getFullName()); //выведет 'Иван Иванов'
+console.log(student.year); //выведет 2017
+console.log(student.getCourse()); //выведет 3 - третий курс, так как текущий год 2020
